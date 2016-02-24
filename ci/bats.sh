92450e6c654f6ec85c074610f02eacfefd4fab50
jianqiu <wangjq@cn.ibm.com>
2016-02-24 11:11:12 +0800

    Updated bats.sh

--- ci/bats.sh
+++ ci/bats.sh
@@ -5,9 +5,9 @@ set -e -x
 source $(dirname $0)/lib/vagrant.sh
 source $(dirname $0)/lib/bats.sh
 
-stemcell_url=`cat warden-ubuntu-trusty-stemcell/url`
+stemcell_url=`cat softlayer-ubuntu-trusty-stemcell/url`
 bosh_release_path=$PWD/bosh-release/*.tgz
-cpi_release_path=$PWD/pipeline-bosh-warden-cpi-tarball/*.tgz
+cpi_release_path=$PWD/pipeline-bosh-softlayer-cpi-tarball/*.tgz
 garden_linux_release_path=$PWD/garden-linux-release/*.tgz
 
 vagrant_up
