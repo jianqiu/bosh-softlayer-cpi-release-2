07e2b0734997d5e6e7a73c4fcdb18652cec66dcd
jianqiu <wangjq@cn.ibm.com>
2016-02-25 15:26:51 +0800

    Refined softlayer cpi release pipeline

---
+++ ci/tasks/build-candidate.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+semver=`cat version-semver/number`
+
+mkdir out
+
+cd bosh-cpi-release
+
+source .envrc
+
+echo "running unit tests"
+./src/github.com/maximilien/bosh-softlayer-cpi/bin/test
+
+echo "using bosh CLI version..."
+bosh version
+
+cpi_release_name="bosh-softlayer-cpi"
+
+echo "building CPI release..."
+bosh create release --name $cpi_release_name --version $semver --with-tarball
+
+mv dev_releases/$cpi_release_name/$cpi_release_name-$semver.tgz ../out/
