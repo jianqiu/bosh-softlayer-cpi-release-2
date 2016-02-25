26d8da668d179cf5b8e0ef610f2b000115f58edf
jianqiu <wangjq@cn.ibm.com>
2016-02-25 22:00:32 +0800

    Update scripts

--- ci/tasks/teardown-director.sh
+++ ci/tasks/teardown-director.sh
@@ -5,8 +5,7 @@ set -e
 source /etc/profile.d/chruby.sh
 chruby 2.1.2
 
-initver=$(cat setup-director/bosh-init/version)
-initexe="${PWD}/setup-director/bosh-init/bosh-init-${initver}-linux-amd64"
+initexe="${PWD}/setup-director/bosh-init/bosh-init"
 chmod +x ${initexe}
 
 echo "using bosh-init CLI version..."
